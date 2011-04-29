8d158dc315eeadd6e512fdda84baaa2207e39944
Thomas Botton <thomas.botton@gmail.com>
2011-04-29 16:42:06 +0200

    Updating to Symfony Beta 1 from PR10

--- bin/vendors.sh
+++ bin/vendors.sh
@@ -3,6 +3,7 @@
 DIR=`php -r "echo dirname(dirname(realpath('$0')));"`
 VENDOR="$DIR/vendor"
 VERSION=`cat "$DIR/VERSION"`
+BUNDLES=$VENDOR/bundles
 
 # initialization
 if [ "$1" = "--reinstall" -o "$2" = "--reinstall" ]; then
@@ -19,7 +20,7 @@ mkdir -p "$VENDOR" && cd "$VENDOR"
 
 ##
 # @param destination directory (e.g. "doctrine")
-# @param URL of the git remote (e.g. git://github.com/doctrine/doctrine2.git)
+# @param URL of the git remote (e.g. http://github.com/doctrine/doctrine2.git)
 # @param revision to point the head (e.g. origin/HEAD)
 #
 install_git()
@@ -27,6 +28,8 @@ install_git()
     INSTALL_DIR=$1
     SOURCE_URL=$2
     REV=$3
+
+    echo "> Installing/Updating " $INSTALL_DIR
 
     if [ -z $REV ]; then
         REV=origin/HEAD
@@ -43,52 +46,51 @@ install_git()
 }
 
 # Assetic
-install_git assetic git://github.com/kriswallsmith/assetic.git #v1.0.0alpha1
+install_git assetic http://github.com/kriswallsmith/assetic.git
 
 # Symfony
-install_git symfony git://github.com/symfony/symfony.git #v$VERSION
+install_git symfony http://github.com/symfony/symfony.git #v$VERSION
+
+# Doctrine ORM
+install_git doctrine http://github.com/doctrine/doctrine2.git 2.0.4
+
+# Doctrine DBAL
+install_git doctrine-dbal http://github.com/doctrine/dbal.git 2.0.4
+
+# Doctrine Common
+install_git doctrine-common http://github.com/doctrine/common.git 2.0.2
+
+# Swiftmailer
+install_git swiftmailer http://github.com/swiftmailer/swiftmailer.git origin/4.1
+
+# Twig
+install_git twig http://github.com/fabpot/Twig.git
+
+# Twig Extensions
+install_git twig-extensions http://github.com/fabpot/Twig-extensions.git
+
+# Monolog
+install_git monolog http://github.com/Seldaek/monolog.git
+
+# SensioFrameworkExtraBundle
+mkdir -p $BUNDLES/Sensio/Bundle
+cd $BUNDLES/Sensio/Bundle
+install_git FrameworkExtraBundle http://github.com/sensio/SensioFrameworkExtraBundle.git
+
+# SecurityExtraBundle
+mkdir -p $BUNDLES/JMS
+cd $BUNDLES/JMS
+install_git SecurityExtraBundle http://github.com/schmittjoh/SecurityExtraBundle.git
+
+# Symfony bundles
+mkdir -p $BUNDLES/Symfony/Bundle
+cd $BUNDLES/Symfony/Bundle
+
+# WebConfiguratorBundle
+install_git WebConfiguratorBundle http://github.com/symfony/WebConfiguratorBundle.git
 
 # Update the bootstrap files
 $DIR/bin/build_bootstrap.php
-
-# Doctrine ORM
-install_git doctrine git://github.com/doctrine/doctrine2.git 2.0.3
-
-# Doctrine DBAL
-install_git doctrine-dbal git://github.com/doctrine/dbal.git 2.0.3
-
-# Doctrine Common
-install_git doctrine-common git://github.com/doctrine/common.git 2.0.1
-
-# Swiftmailer
-install_git swiftmailer git://github.com/swiftmailer/swiftmailer.git origin/4.1
-
-# Twig
-install_git twig git://github.com/fabpot/Twig.git v1.0.0
-
-# Twig Extensions
-install_git twig-extensions git://github.com/fabpot/Twig-extensions.git
-
-# Monolog
-install_git monolog git://github.com/Seldaek/monolog.git
-
-# SensioFrameworkExtraBundle
-mkdir -p bundles/Sensio/Bundle
-cd bundles/Sensio/Bundle
-install_git FrameworkExtraBundle git://github.com/sensio/SensioFrameworkExtraBundle.git
-cd ../../..
-
-# SecurityExtraBundle
-mkdir -p bundles/JMS
-cd bundles/JMS
-install_git SecurityExtraBundle git://github.com/schmittjoh/SecurityExtraBundle.git
-cd ../..
-
-# WebConfiguratorBundle
-mkdir -p bundles/Symfony/Bundle
-cd bundles/Symfony/Bundle
-install_git WebConfiguratorBundle git://github.com/symfony/WebConfiguratorBundle.git
-cd ../../..
 
 # Update assets
 $DIR/app/console assets:install $DIR/web/
